0a6f6311f56dbeaeaac698fbe532d2cf6ae468d7
Justin Carruthers <Jtcruthers@gmail.com>
2021-06-24 03:42:39 -0400

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,28 @@
-#! /bash/sh
+#! /bin/bash
 
+rm -f ~/.zshrc
+rm -f ~/.vimrc
+rm -f ~/.tmux.conf
 rm -f ~/.yabairc
 rm -f ~/.skhdrc
-rm -f ~/.tmux.conf
-rm -f ~/.config/alacritty/alacritty.xml
+rm -f ~/.config/alacritty/alacritty.yml
+rm -f ~/.config/karabiner/karabiner.json
 
 # Install configs
-ln -s "${HOME}/.config/yabai/yabairc" "${HOME}/.yabairc"
-ln -s "${HOME}/.config/yabai/skhdrc" "${HOME}/.skhdrc"
+JDOT_HOME="${HOME}/.config/jdot"
+
+git clone git@github.com:Jtcruthers/dotfiles.git $JDOT_HOME
+
+DOTFILES="${JDOT_HOME}/dotfiles"
+
+ln -s "${DOTFILES}/yabairc" "${HOME}/.yabairc"
+ln -s "${DOTFILES}/skhdrc" "${HOME}/.skhdrc"
+ln -s "${DOTFILES}/tmux.conf" "${HOME}/.tmux.conf"
+ln -s "${DOTFILES}/zshrc" "${HOME}/.zshrc"
+ln -s "${DOTFILES}/vimrc" "${HOME}/.vimrc"
+
+mkdir -p ~/.config/alacritty
+ln -s "${DOTFILES}/alacritty.yml" ~/.config/alacritty/alacritty.yml
+
+mkdir -p ~/.config/karabiner
+ln -s "${DOTFILES}/karabiner.json" ~/.config/karabiner/karabiner.json

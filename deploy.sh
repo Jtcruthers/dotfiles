90a1521fa9ba6d58d061c59f305c83ac691f4857
Justin Carruthers <Jtcruthers@gmail.com>
2021-06-24 22:50:31 -0400

    Added p10k

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ rm -f ~/.yabairc
 rm -f ~/.skhdrc
 rm -f ~/.config/alacritty/alacritty.yml
 rm -f ~/.config/karabiner/karabiner.json
+rm -f ~/.p10k.zsh
 
 # Install configs
 JDOT_HOME="${HOME}/.config/jdot"
@@ -20,6 +21,7 @@ ln -s "${DOTFILES}/skhdrc" "${HOME}/.skhdrc"
 ln -s "${DOTFILES}/tmux.conf" "${HOME}/.tmux.conf"
 ln -s "${DOTFILES}/zshrc" "${HOME}/.zshrc"
 ln -s "${DOTFILES}/vimrc" "${HOME}/.vimrc"
+ln -s "${DOTFILES}/p10k.zsh" "${HOME}/.p10k.zsh"
 
 mkdir -p ~/.config/alacritty
 ln -s "${DOTFILES}/alacritty.yml" ~/.config/alacritty/alacritty.yml

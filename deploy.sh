ae947b4e6a75f77acf0c876d614fcabd3f9d95c5
Justin Carruthers <Jtcruthers@gmail.com>
2024-03-01 03:06:20 -0500

    added coc-settings.json

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ rm -f ~/.p10k.zsh
 rm -f ~/.gitconfig
 rm -f ~/.config/alacritty/alacritty.yml
 rm -f ~/.config/nvim/init.vim
+rm -f ~/.config/nvim/coc-settings.json
 
 # Install configs
 JDOT_HOME="${HOME}/.config/dotfiles"
@@ -17,4 +18,5 @@ ln -s "${JDOT_HOME}/dotfiles/tmux.conf" ~/.tmux.conf
 ln -s "${JDOT_HOME}/dotfiles/p10k.zsh" ~/.p10k.zsh
 ln -s "${JDOT_HOME}/dotfiles/gitconfig" ~/.gitconfig
 ln -s "${JDOT_HOME}/dotfiles/init.vim" ~/.config/nvim/init.vim
+ln -s "${JDOT_HOME}/dotfiles/coc-settings.json" ~/.config/nvim/coc-settings.json
 ln -s "${JDOT_HOME}/dotfiles/alacritty.yml" ~/.config/alacritty/alacritty.yml

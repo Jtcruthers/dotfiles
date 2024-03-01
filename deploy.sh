f089964a82d1fd191aa7a2f44b8180dae14183d1
Justin Carruthers <Jtcruthers@gmail.com>
2024-03-01 03:15:13 -0500

    add prompt on deploy script since it deletes files

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,25 @@
 #! /bin/bash
 
+# Prompt for confirmation
+echo "This script deletes existing config files. Continue?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) echo "Installing..."; break;;
+        No ) echo "Exiting..."; exit;;
+    esac
+done
+
+# Install configs
+JDOT_HOME="${HOME}/.dotfiles"
+
+if [ -d $JDOT_HOME ]; then
+  echo "$JDOT_HOME already exists, skipping repo clone."
+else
+  echo "Cloning Jtcruthers/dotfiles.git to $JDOT_HOME"
+  git clone git@github.com:Jtcruthers/dotfiles.git $JDOT_HOME
+fi
+
+echo "Deleting old configs"
 rm -f ~/.zshrc
 rm -f ~/.tmux.conf
 rm -f ~/.p10k.zsh
@@ -8,16 +28,7 @@ rm -f ~/.config/alacritty/alacritty.yml
 rm -f ~/.config/nvim/init.vim
 rm -f ~/.config/nvim/coc-settings.json
 
-# Install configs
-JDOT_HOME="${HOME}/.dotfiles"
-
-if [ -d $JDOT_HOME ]; then
-  echo "$JDOT_HOME already exists"
-else
-  echo "Cloning Jtcruthers/dotfiles.git to $JDOT_HOME"
-  git clone git@github.com:Jtcruthers/dotfiles.git $JDOT_HOME
-fi
-
+echo "Adding soft links to configs"
 ln -s "${JDOT_HOME}/dotfiles/zshrc" ~/.zshrc
 ln -s "${JDOT_HOME}/dotfiles/tmux.conf" ~/.tmux.conf
 ln -s "${JDOT_HOME}/dotfiles/p10k.zsh" ~/.p10k.zsh

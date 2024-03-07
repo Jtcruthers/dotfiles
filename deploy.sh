02ad58620a39347905f5fa1da349f3e8480a64f8
Justin Carruthers <Jtcruthers@gmail.com>
2024-03-06 22:51:42 -0500

    alacritty.yml -> alacritty.toml

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ rm -f ~/.zshrc
 rm -f ~/.tmux.conf
 rm -f ~/.p10k.zsh
 rm -f ~/.gitconfig
-rm -f ~/.config/alacritty/alacritty.yml
+rm -f ~/.config/alacritty/alacritty.toml
 rm -f ~/.config/nvim/init.vim
 rm -f ~/.config/nvim/coc-settings.json
 
@@ -35,4 +35,4 @@ ln -s "${JDOT_HOME}/dotfiles/p10k.zsh" ~/.p10k.zsh
 ln -s "${JDOT_HOME}/dotfiles/gitconfig" ~/.gitconfig
 ln -s "${JDOT_HOME}/dotfiles/init.vim" ~/.config/nvim/init.vim
 ln -s "${JDOT_HOME}/dotfiles/coc-settings.json" ~/.config/nvim/coc-settings.json
-ln -s "${JDOT_HOME}/dotfiles/alacritty.yml" ~/.config/alacritty/alacritty.yml
+ln -s "${JDOT_HOME}/dotfiles/alacritty.toml" ~/.config/alacritty/alacritty.toml

b417012a77289e860f0eb86e0c2d5da8778b824b
Justin Carruthers <Jtcruthers@gmail.com>
2025-02-01 01:04:06 -0500

    added lazy-lock.json

--- dotfiles/.config/nvim/lua/user/lsp.lua
+++ dotfiles/.config/nvim/lua/user/lsp.lua
@@ -3,7 +3,7 @@ require("neoconf").setup {}
 
 vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
 vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>') 
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
 
 vim.api.nvim_create_autocmd('LspAttach', {
   desc = 'LSP actions',

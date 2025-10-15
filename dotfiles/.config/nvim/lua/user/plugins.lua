338d61153c2e7aa00f24e2d6139ed6c8ce6862df
Justin Carruthers <Jtcruthers@gmail.com>
2025-10-15 13:35:16 -0400

    added plugins for claude code

--- dotfiles/.config/nvim/lua/user/plugins.lua
+++ dotfiles/.config/nvim/lua/user/plugins.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
   { "numToStr/Comment.nvim" },
   { "JoosepAlviste/nvim-ts-context-commentstring" },
   { "kyazdani42/nvim-web-devicons" },
-  { "nvim-treesitter/nvim-treesitter", build = ':TSUpdate' },
+  { "nvim-treesitter/nvim-treesitter",            build = ':TSUpdate' },
   {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
@@ -34,11 +34,11 @@ require("lazy").setup({
   "folke/neoconf.nvim",
 
   -- UI
-  { "akinsho/bufferline.nvim", version = "*" },
+  { "akinsho/bufferline.nvim",  version = "*" },
   { "nvim-lualine/lualine.nvim" },
   { "lewis6991/gitsigns.nvim" },
   { "qpkorr/vim-bufkill" },
-  { "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" },
+  { "moll/vim-bbye",            commit = "25ef93ac5a87526111f43e5110675032dbcacf56" },
   { "RRethy/vim-illuminate" },
   {
     "akinsho/toggleterm.nvim",
@@ -51,26 +51,36 @@ require("lazy").setup({
   },
   {
     "goolord/alpha-nvim",
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    config = function()
+      require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
     end
   },
- "folke/which-key.nvim",
+  "folke/which-key.nvim",
 
   -- LSP
   "williamboman/mason.nvim", -- simple to use language server installer
   "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig", -- enable LSP
+  "neovim/nvim-lspconfig",   -- enable LSP
   "darrikonn/vim-gofmt",
+  {
+    "OlegGulevskyy/better-ts-errors.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    config = {
+      keymaps = {
+        toggle = '<leader>ww',          -- default '<leader>dd'
+        go_to_definition = '<leader>wd' -- default '<leader>dx'
+      }
+    }
+  },
 
   -- Completion
-  { "hrsh7th/nvim-cmp" }, -- The completion plugin
+  { "hrsh7th/nvim-cmp" },   -- The completion plugin
   { "hrsh7th/cmp-buffer" }, -- buffer completions
-  { "hrsh7th/cmp-path" }, -- path completions
+  { "hrsh7th/cmp-path" },   -- path completions
   { "hrsh7th/cmp-nvim-lsp" },
   { "hrsh7th/cmp-nvim-lua" },
-  { "saadparwaiz1/cmp_luasnip" }, -- snippet completions
-  { "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" }, --snippet engine
+  { "saadparwaiz1/cmp_luasnip" },                                            -- snippet completions
+  { "L3MON4D3/LuaSnip",            version = "v2.*", build = "make install_jsregexp" }, --snippet engine
   { "rafamadriz/friendly-snippets" },
   {
     "zbirenbaum/copilot.lua",
@@ -79,23 +89,32 @@ require("lazy").setup({
     lazy = true,
   },
   "zbirenbaum/copilot-cmp",
+  {
+    "greggh/claude-code.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- Required for git operations
+    },
+    config = function()
+      require("claude-code").setup()
+    end
+  },
 
 
   -- Telescope
-  { "nvim-telescope/telescope.nvim", branch = "0.1.x" },
+  { "nvim-telescope/telescope.nvim",            branch = "0.1.x" },
   { "nvim-telescope/telescope-media-files.nvim" },
   { "nvim-telescope/telescope-fzf-native.nvim", build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
   "camgraff/telescope-tmux.nvim",
 
   { "christoomey/vim-tmux-navigator" },
 
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim",               name = "catppuccin", priority = 1000 },
 
   -- Backend
   {
     'kristijanhusak/vim-dadbod-ui',
     dependencies = {
-      { 'tpope/vim-dadbod', lazy = true },
+      { 'tpope/vim-dadbod',                     lazy = true },
       { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
     },
     cmd = {

e5946149210e098e6b5d24e4b6953ec7ee3b3bfe
Justin Carruthers <Jtcruthers@gmail.com>
2025-10-15 14:04:59 -0400

    Updated fish config to be cross platform

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -13,7 +13,7 @@ end
 set --erase _asdf_shims
 
 # pnpm
-set -gx PNPM_HOME "/Users/justin/Library/pnpm"
+set -gx PNPM_HOME "$HOME/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
--- dotfiles/.config/fish/functions/remotedev.fish
+++ dotfiles/.config/fish/functions/remotedev.fish
@@ -1,4 +1,3 @@
 function remotedev --description 'SSH into dev EC2 host'
-  #function remotedev --wraps='ssh -i /Users/Justin/vv/devrds.pem ec2-user@ec2-3-220-167-27.compute-1.amazonaws.com' --description 'SSH into dev EC2 host'
-  ssh -i /Users/Justin/vv/devrds.pem ec2-user@ec2-44-198-54-5.compute-1.amazonaws.com 
+  ssh -i $HOME/vv/devrds.pem ec2-user@ec2-44-198-54-5.compute-1.amazonaws.com 
 end
